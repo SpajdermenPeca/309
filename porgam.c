cc2280919e7dc7a20dab140654770ff17576d6ea
Gladius Romae <0410923@teslabg.edu.rs>
2026-04-08 12:26:21 +0200

    Update porgam.c

--- porgam.c
+++ porgam.c
@@ -4,5 +4,7 @@
 int main()
 {
   printf("Ja sam klas jan ćelardu pošmrko sam petardu");
+  printf("Valjao sam robu pokojnom dirty bastardu");
+  printf("Pozdravljam SKIZ gardu, pozdrav i za šokaru");
   return 0;
 }
